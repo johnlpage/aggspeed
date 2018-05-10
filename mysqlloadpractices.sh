a8f73d486866a33c067cf0ce2f1d0e91411d1cb5
John Page <johnlpage@gmail.com>
2018-05-10 15:08:59 -0400

    added auth

--- mysqlloadpractices.sh
+++ mysqlloadpractices.sh
@@ -1,6 +1,6 @@
 
-mysql -u root  -e ' DROP TABLE practices' nhs
-mysql -u root -e ' CREATE TABLE practices (period int,code char(6),name varchar(42),add1 varchar(30), add2 varchar(30),town varchar(30),county varchar(30),postcode char(9), primary key (code,period));' nhs
+mysql -u root -ppassword  -e ' DROP TABLE practices' nhs
+mysql -u root -ppassword -e ' CREATE TABLE practices (period int,code char(6),name varchar(42),add1 varchar(30), add2 varchar(30),town varchar(30),county varchar(30),postcode char(9), primary key (code,period));' nhs
 
 
 
@@ -8,6 +8,6 @@ for f in Practises_*.csv
 do
 		echo $f
 		if [ ! -f "$f.loaded" ]; then
-        	mysql -u root  -e  "LOAD DATA LOCAL INFILE '"$f"' INTO TABLE practices FIELDS TERMINATED BY ',' IGNORE 1 LINES" nhs && touch "$f.loaded"
+        	mysql -ppassword -u root  -e  "LOAD DATA LOCAL INFILE '"$f"' INTO TABLE practices FIELDS TERMINATED BY ',' IGNORE 1 LINES" nhs && touch "$f.loaded"
         fi
 done
--- mysqlloadpresriptions.sh
+++ mysqlloadpresriptions.sh
@@ -1,13 +1,13 @@
-mysql  -u root -e "CREATE DATABASE nhs;"
+mysql  -u root -ppassword -e "CREATE DATABASE nhs;"
 
 
-mysql -u root -e ' CREATE TABLE prescriptions ( sha char(3),pct char(3),practice char(6), bnfcode char(16),name varchar(32),nitems integer,nic real,cost real,quantity integer,period integer, primary key(practice,period,bnfcode));' nhs
+mysql -u root -ppassword -e ' CREATE TABLE prescriptions ( sha char(3),pct char(3),practice char(6), bnfcode char(16),name varchar(32),nitems integer,nic real,cost real,quantity integer,period integer, primary key(practice,period,bnfcode));' nhs
 
 
 for f in Prescriptions*.csv
 do
 		echo $f
 		if [ ! -f "$f.loaded" ]; then
-        	mysql -u root  -e  "LOAD DATA LOCAL INFILE '"$f"' INTO TABLE prescriptions FIELDS TERMINATED BY ',' IGNORE 1 LINES" nhs && touch "$f.loaded"
+        	mysql -u root -ppassword  -e  "LOAD DATA LOCAL INFILE '"$f"' INTO TABLE prescriptions FIELDS TERMINATED BY ',' IGNORE 1 LINES" nhs && touch "$f.loaded"
         fi
 done
